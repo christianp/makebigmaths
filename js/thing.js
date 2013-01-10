7d102e1bf236a24a12b783cb16a1f5b944bc0c23
Christian Perfect <christianperfect@gmail.com>
2013-01-10 08:01:29 +0000

    add credit

--- js/thing.js
+++ js/thing.js
@@ -12,7 +12,7 @@ function display() {
 	$('#output').html('\\['+tex+'\\]');
 	MathJax.Hub.Queue(['Typeset',MathJax.Hub]);
 	if (!window.location.origin) window.location.origin = window.location.protocol+"//"+window.location.host;
-	var url = window.location.origin+window.location.pathname+'?'+encodeURIComponent(tex);
+	var url = window.location.origin+window.location.pathname+(tex.length ? '?'+encodeURIComponent(tex) : '');
 	$('#link').text(url).attr('href',url);
 }
 
@@ -23,6 +23,7 @@ $(function() {
 		$('#output').css('font-size',size+'em');
 		display();
 	});
+	display();
 	if(location.search.length)
 		$('#input #tex').val(unescape(location.search.slice(1))).change();
 });
